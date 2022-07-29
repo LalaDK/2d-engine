ecd88ba797d05b6fcddf63d375557a02995156b2
Mads Dam Hvidbjerg Eckardt <maeck@eg.dk>
2022-07-29 11:20:23 +0200

    more

--- src/Physics/Force.cpp
+++ src/Physics/Force.cpp
@@ -13,3 +13,14 @@ Vec2 Force ::GenerateDragForce(const Particle& particle, float k)
 
     return dragForce;
 };
+
+Vec2 Force ::GenerateFrictionForce(const Particle& particle, float k)
+{
+    Vec2 frictionForce = Vec2(0,0);
+    Vec2 frictionDirection = particle.velocity.UnitVector() * -1.0;
+    float frictionMagnitude = k;
+    frictionForce = frictionDirection * dragMagnitude;
+
+
+    return frictionForce;
+};
